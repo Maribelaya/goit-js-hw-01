9d598958677fd8365794d88a2b62b338ed19a846
Maribelaya <shandermarina7@gmail.com>
2023-11-21 13:14:06 +0100

    hw1

--- js/task-1.js
+++ js/task-1.js
@@ -3,8 +3,7 @@ let totalPrice = 0;
 
 totalPrice = quantity * pricePerDroid;
 
-return "You ordered "  + quantity + " droids worth " + totalPrice + " credits!"
-
+return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
